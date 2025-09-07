c4e6dc0fbbe30e622ff4f36ccc91d71a5796ca24
Vitaly Shubin <shubin.vitaly.and@gmail.com>
2025-09-07 17:33:27 +0400

    Add display value format

--- OpcDomain/ValueFormat.cs
+++ OpcDomain/ValueFormat.cs
@@ -1,4 +1,6 @@
-﻿namespace OpcDomain;
+﻿using System.Text.Json;
+
+namespace OpcDomain;
 
 public static class ValueFormat
 {
@@ -6,7 +8,46 @@ public static class ValueFormat
 
     public static (string Display, string? DataType) ToDisplay(OpcTag tag)
     {
-        throw new NotImplementedException();
+        if (tag.StatusCode.ToLower() == "badnodeidunknown"
+            || tag.Value.ValueKind == JsonValueKind.Null)
+        {
+            return (Constants.NoSuchTag, tag.DataType);
+        }
+
+        var dt = tag.DataType ?? DisplayDataType(tag);
+
+        if (dt?.ToLower() == "int32"
+            && tag.Attributes != null &&
+            tag.Attributes.EnumStrings is { Length: > 0 }
+            && tag.Value.TryGetInt32(out var iv))
+        {
+            var str = (iv >= 0 && iv < tag.Attributes.EnumStrings.Length) ?
+                tag.Attributes.EnumStrings[iv] :
+                iv.ToString();
+
+            return (str, dt);
+        }
+
+        if (tag.ArrayType?.ToLower() == "array")
+        {
+            var arr = tag.Value;
+
+            if (arr.ValueKind == JsonValueKind.Array)
+            {
+                var len = arr.GetArrayLength();
+
+                string data = len > 0 ?
+                    string.Join(", ", arr.EnumerateArray()
+                        .Take(ArrayLimith)
+                        .Select(e => e.ToString())) :
+                    "";
+                var preview = len > ArrayLimith ? $"{data}, ..." : data;
+
+                return ($"[{preview}] (len={len})", dt);
+            }
+        }
+
+        return (tag.Value.ToString(), dt);
     }
 
     public static TimeSpan? AgeUtc(OpcTag tag, DateTime nowUtc)
@@ -17,5 +58,26 @@ public static class ValueFormat
         }
 
         return nowUtc - tag.ServerTimestamp.Value;
+    }
+
+    private static string? DisplayDataType(OpcTag tag)
+    {
+        if (tag.DisplayName.ToLower() == "nosuchtag")
+        {
+            return Constants.NoSuchTag;
+        }
+
+        switch (tag.Value.ValueKind)
+        {
+            case JsonValueKind.String:
+                return "String";
+            case JsonValueKind.Number:
+                return "Double";
+            case JsonValueKind.True:
+            case JsonValueKind.False:
+                return "Boolean";
+            default:
+                return tag.DataType;
+        }
     }
 }
