3b509338e227158a9c125415bf7562318b09c14e
Vitaly Shubin <shubin.vitaly.and@gmail.com>
2025-09-07 15:55:24 +0400

    Add unit-tests

---
+++ OpcDomain/ValueFormat.cs
@@ -0,0 +1,16 @@
+﻿namespace OpcDomain;
+
+public static class ValueFormat
+{
+    public const int ArrayLimith = 1;
+
+    public static (string Display, string? DataType) ToDisplay(OpcTag tag)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static TimeSpan? AgeUtc(OpcTag tag, DateTime nowUtc)
+    {
+        throw new NotImplementedException();
+    }
+}
